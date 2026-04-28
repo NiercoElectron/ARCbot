1bfc67e4a54921d3188e99b1e8b2c5d92a02b1e3
Gustavo <Gustavohiruo@gmail.com>
2026-04-28 18:45:53 -0300

    tirando o funcionalidade

--- cogs/general.py
+++ cogs/general.py
@@ -14,22 +14,6 @@ class General(commands.Cog):
     async def ping(self, ctx: commands.Context):
         await ctx.send('Pong!')
 
-    @commands.command()
-    async def chamar(self, ctx: commands.Context, member: discord.Member, qnt: int):
-        if qnt < 1:
-            await ctx.send('Se você quer chamar alguém, com certeza é para chamar mais de uma vez, né?')
-            return
-        if qnt > 50:
-            await ctx.send('Zero é pouco e 50 é demais, vamos manter a quantidade entre 1 e 50 para evitar spam.')
-            return
-        if member == ctx.author:
-            await ctx.send('Você não pode se mencionar, escolha outra pessoa para chamar!')
-            return
-        if member == self.bot.user:
-            await ctx.send('Eu sou um bot, não posso ser mencionado dessa forma! Escolha outra pessoa para chamar!')
-            return
-        await ctx.send(f'Vítima da vez: {member.mention}, mencionado {qnt} vezes! \n{member.mention * qnt}')
-
     @chamar.error
     async def chamar_error(self, ctx: commands.Context, error):
         if isinstance(error, commands.MemberNotFound):
